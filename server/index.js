f4b79c335d827102b80ae5607d730554aa12ff23
Mehrin Muhammed <mehrinmuhammed2001@gmail.com>
2024-03-24 14:02:38 +0530

    connected dbase

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(9001,()=>{
-    console.log("server is listeneing")
-})
+// MONGODB CONNECT
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(9001, () => {
+  console.log("server is listeneing");
+});
